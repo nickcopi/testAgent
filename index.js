7fc66c578ff9bfa893b75c9174f3969de22d106a
Nick <>
2019-05-28 16:39:48 -0400

    Dibs work and stuff.

--- index.js
+++ index.js
@@ -7,18 +7,22 @@ const greenGuy = 'http://localhost:8080';
 let buildTime = async ()=>{
 	let queue = JSON.parse(await request(greenGuy + '/getTestQueue'));
 	console.log(queue);
+	let dontTest = false;
 	await Promise.all(queue.map( async q=>{
 		let name = q.name;
 		let version = q.version;
 		let pkgName =`${name}.${version}.nupkg`; 
+		if(q.dibs) return dontTest = true;
+		let dibsStatus = JSON.parse( await callDibs(name));
+		if(!dibsStatus.success) return dontTest = true;
 		const options = {
 			encoding:null,
 			url:`${greenGuy}/packages/${name}/${pkgName}`
 		}
 		let data = await request(options).catch(e=>console.log(e));
 		fs.writeFileSync(pkgName,data);
-		return q;
 	}));
+	if(dontTest) return;
 	await Promise.all(queue.map(async q=>{
 		const pkgName =`${q.name}.${q.version}.nupkg`; 
 		try{
@@ -35,12 +39,12 @@ let buildTime = async ()=>{
 			console.log(e);
 			sendReport(q.name,false,e,e);
 		}
-		return q;
 	}));
-console.log('Done');
+	console.log('Done');
 
 }
 let sendReport = (name,success,error,result)=>{
+	console.log(result);
 	const options = {
 		method:'POST',
 		url:greenGuy + '/agentReport',
@@ -63,10 +67,34 @@ let didInstall = result=>{
 			l.split(' ').forEach(w=>{
 				if(w.includes('/')){
 					if(w.split('/')[0] === w.split('/')[1]) success = true;
+					if(w.split('/')[0] == 0) success = false;
 				}
 			});
 		}
 	});
 	return success;
 }
+let callDibs = async name =>{
+	const options = {
+		method:'POST',
+		url:greenGuy + '/agentDibs',
+		headers:{
+			'Content-Type':'application/json'
+		},
+		body:JSON.stringify({
+			name,
+		})
+	}
+	let res = await request(options);
+	return res;
+	
+
+}
+let startSchedule = ()=>{
+	schedule.scheduleJob('* * * * *',()=>{
+		buildTime();
+	});
+};
 buildTime();
+startSchedule();
+
